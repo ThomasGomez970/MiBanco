25fd7e0f689e327d07f21f8cf972a1e6863053df
ThomasGomez970 <hillstommy97@gmail.com>
2025-08-29 22:49:36 -0500

    Tarea1Mibanco

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+struct Cliente {
+    string nom;
+    int saldo;
+};
+
+typedef struct Cliente Cliente;
+
+void menu() {
+    cout << "MENU MI BANCO" << endl;
+    cout << "1. Agregar cliente" << endl;
+    cout << "2. Listar clientes" << endl;
+    cout << "3. Consignar" << endl;
+    cout << "4. Retirar" << endl;
+    cout << "5. Transferir" << endl;
+    cout << "6. Totales" << endl;
+    cout << "0. Salir" << endl;
+    return;
+}
+
+int main() {
+    int opc, n = 0, suma;
+    Cliente banco[ 100 ];
+    do {
+        string nom, nom2;
+        int monto;
+        menu();
+        cout << "opcion: "; cin >> opc;
+        switch( opc ) {
+        case 1:
+            cout << "Nombre= ";
+            cin >> banco[ n ].nom;
+            banco[ n++ ].saldo = 0;
+            break;
+        case 2:
+            for ( int i = 0; i < n; i++ ) {
+                cout << banco[ i ].nom << " " << banco[ i ].saldo << endl;
+            }
+            break;
+        case 3:
+            cout << "Nombre= "; cin >> nom;
+            for ( int i = 0; i < n; i++ ) {
+                if ( banco[i ].nom == nom ) {
+                    cout << "Monto a consignar="; cin >> monto;
+                    banco[ i ].saldo += monto;
+                }
+            }
+            break;
+        case 4:
+            cout << "Nombre= "; cin >> nom;
+            for ( int i = 0; i < n; i++ ) {
+                if ( banco[i ].nom == nom ) {
+                    cout << "Monto a retirar="; cin >> monto;
+                    banco[ i ].saldo -= monto;
+                }
+            }
+            break;
+        case 5:
+            cout << "Nombre quien transfiere= "; cin >> nom;
+            cout << "Nombre quien recibe= "; cin >> nom2;
+            for ( int i = 0; i < n; i++ ) {
+                if ( banco[i ].nom == nom ) {
+                    for ( int j = 0; j < n; j++ ) {
+                        if ( banco[j ].nom == nom2 ) {
+                            cout << "Monto a transferir="; cin >> monto;
+                            banco[ i ].saldo -= monto;
+                            banco[ j ].saldo += monto;
+                        }
+                    }
+                }
+            }
+            break;
+        case 6:
+            suma = 0;
+            for ( int i = 0; i < n; i++ ) {
+                suma += banco[ i ].saldo;
+            }
+            cout << "El total de los saldos es: " << suma << endl;
+            break;
+        }
+    } while ( opc != 0 );
+
+    return 0;
+}
